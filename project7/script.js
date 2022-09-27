e86904625760ba68ae341ccccbdbe5f02e6bcacb
Varun Pandey <varunpandeyuttrakhand+github@gmail.com>
2022-09-27 21:59:18 +0530

    Update script.js

--- project7/script.js
+++ project7/script.js
@@ -6,11 +6,11 @@ const height = document.createElement("span");
 width.append(body.getBoundingClientRect().width);
 height.append(body.getBoundingClientRect().height);
 
-body.children[0].insertAdjacentElement("beforeend",width);  
-body.children[1].insertAdjacentElement("beforeend",height);
+body.children[0].insertAdjacentElement("beforeend",height);  
+body.children[1].insertAdjacentElement("beforeend",width);
 
 
 window.addEventListener("resize",(e)=>{
-  body.children[0].querySelector('span').textContent = body.getBoundingClientRect().width;  
-  body.children[1].querySelector('span').textContent = body.getBoundingClientRect().height;  
+  body.children[0].querySelector('span').textContent = body.getBoundingClientRect().height;  
+  body.children[1].querySelector('span').textContent = body.getBoundingClientRect().width;  
 });
