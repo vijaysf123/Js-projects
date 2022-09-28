027370d6fc6247bdbd44bcd1ebab3692cf6eedd6
Varun Pandey <varunpandeyuttrakhand+github@gmail.com>
2022-09-28 12:14:35 +0530

    Update script.js

--- project7/script.js
+++ project7/script.js
@@ -6,22 +6,27 @@
         e.preventDefault();
         // get the inpt msg box element         
         const message = document.querySelector("#msgbox");
-        const msgHtml = `${message.value}`;
         
-        // Clear message box
-        message.value = "";
+        if(message.value != ""){
+            const msgHtml = `${message.value}`;
+            
+            // Clear message box
+            message.value = "";
 
-        // print the message         
-        printMsgDiv.textContent = msgHtml;
-        printMsgDiv.classList.add("msg");
+            // print the message         
+            printMsgDiv.textContent = msgHtml;
+            printMsgDiv.classList.add("msg");
         
-        // Focus on that message
-        printMsgDiv.focus();
+            // Focus on that message
+            printMsgDiv.focus();
 
-        // message will show only for 10 seconds then auto disappear               
-        setTimeout(() => {
-            printMsgDiv.textContent = "";
-            printMsgDiv.classList.remove("msg");
-        }, 5000);
+            // message will show only for 10 seconds then auto disappear               
+            setTimeout(() => {
+                printMsgDiv.textContent = "";
+                printMsgDiv.classList.remove("msg");
+            }, 5000);
+        } else {
+            alert("write some message first");
+        }
     })
 })();
