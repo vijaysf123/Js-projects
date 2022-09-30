4a086c97af05c02e7b8cc2e53762d30e8a8f836a
varun-pandey <varunpandeyuttrakhand+chat@gmail>
2022-09-30 15:08:17 +0530

    clock implemented

--- project5/main.js
+++ project5/main.js
@@ -1,20 +1,18 @@
 setInterval(function setTime(e) {
 
-let hoursHand = document.querySelector('#hour-hand');
-let minutesHand = document.querySelector('#minute-hand');
-let secondsHand = document.querySelector('#second-hand');
+    let hoursHand = document.querySelector('#hour-hand');
+    let minutesHand = document.querySelector('#minute-hand');
+    let secondsHand = document.querySelector('#second-hand');
 
 
-let d = new Date();
-let h = d.getHours();
-let m = d.getMinutes();
-let s = d.getSeconds();
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
 
-secondsHand.style.setProperty("--rotation",6*s);
-minutesHand.style.setProperty("--rotation",6*m + s/10);
-hoursHand.style.setProperty("--rotation",30*h + m/2 + s/120);
+    secondsHand.style.setProperty("--rotation", 6 * s);
+    minutesHand.style.setProperty("--rotation", 6 * m + s / 10);
+    hoursHand.style.setProperty("--rotation", 30 * h + m / 2 + s / 120);
 
 
-},1000);	
-
-setTime();
+}, 1000);
